4ac86dcf3b7d19122230bd187627869461fc242c
Mesquita <lordm3sk@gmail.com>
2016-06-06 16:53:31 -0300

    incrementando página inicial

--- application/views/paginasStaticas/inicio.php
+++ application/views/paginasStaticas/inicio.php
@@ -1,5 +1,20 @@
- 
-    <h1>Doar é um ato único de Solidariedade</h1>
-    <h3>Para quem doa, a sensação de dever cumprido<br>
-    Para quem recebe, a felicidade de ter o que precisa</h3>
+<div class="container">
+
+
+    <div class="hero-unit">
+        <h1>Doar é um ato único de Solidariedade</h1>
+        <h3>Para quem doa, a sensação de dever cumprido<br>
+            Para quem recebe, a felicidade de ter o que precisa</h3>
+    </div>
+<div class="row"> 
+    <div class="col-md-4"><img src="http://ibc.org.br/wp-content/blogs.dir/11/files/2014/11/voluntariado-11.jpg"
+                               class="img-circle" alt="Cinque Terre" width="304" height="236">
+        <p>Seja um voluntario. <br> Doe seu tempo e seu conhecimento</div>
+    <div class="col-md-4"> <img src="http://www.1papacaio.com.br/modules/Cliparts/gallery/cliparts_variados/transportes/caminhoes/caminhao007.png"
+                                class="img-circle" alt="Cinque Terre" width="304" height="236"><p>Retiramos roupas, alimentos, moveis, entre outros objetos. <br>
+            Cadastre-se e ajude quem precisa diretamente do conforto da sua casa </div>
+    
+    <div class="col-md-4"><img src="https://static.cineclick.com.br/sites/adm/uploads/banco_imagens/31/602x0_1439644246.jpg"
+                               class="img-circle" alt="Cinque Terre" width="304" height="236"><p>Alguma coisa </div>
+</div>
 
