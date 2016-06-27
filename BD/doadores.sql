33f13915ed71141ba39feaf8d894d6935266dd6f
xulipaxulepa <aofnas@hotmail.com>
2016-06-27 16:44:45 -0300

    exportando o DB

---
+++ BD/doadores.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: 27/06/2016 às 16:40
+-- Versão do servidor: 5.5.47-0ubuntu0.14.04.1
+-- Versão do PHP: 5.5.9-1ubuntu4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de dados: `sistemasolidario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `doadores`
+--
+
+CREATE TABLE IF NOT EXISTS `doadores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `endereço` varchar(150) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `dataNascimento` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
