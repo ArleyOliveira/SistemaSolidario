6fda167cad2139935f5df04df9e03870462dc631
josimarspj <josimarspj33@gmail.com>
2016-07-04 18:07:41 -0300

    Criação da Tabela instituicoes

--- BD/sistemasolidario.sql
+++ BD/sistemasolidario.sql
@@ -1,32 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
 --
--- Servidor: localhost
--- Tempo de Geração: 29/06/2016 às 16:45
--- Versão do servidor: 5.5.47-0ubuntu0.14.04.1
--- Versão do PHP: 5.5.9-1ubuntu4.17
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: sistemasolidario
+-- ------------------------------------------------------
+-- Server version	5.5.49-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `sistemasolidario`
+-- Table structure for table `doadores`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `doadores`
---
-
-CREATE TABLE IF NOT EXISTS `doadores` (
+DROP TABLE IF EXISTS `doadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doadores` (
   `id` int(11) NOT NULL,
   `nome` varchar(40) NOT NULL,
   `endereço` varchar(150) NOT NULL,
@@ -36,14 +32,54 @@ CREATE TABLE IF NOT EXISTS `doadores` (
   `telefone` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `usuario`
+-- Dumping data for table `doadores`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
+LOCK TABLES `doadores` WRITE;
+/*!40000 ALTER TABLE `doadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `instituicoes`
+--
+
+DROP TABLE IF EXISTS `instituicoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `instituicoes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `cnpj` varchar(30) DEFAULT NULL,
+  `telefone` varchar(14) DEFAULT NULL,
+  `email` varchar(40) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `isdisponivel` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `instituicoes`
+--
+
+LOCK TABLES `instituicoes` WRITE;
+/*!40000 ALTER TABLE `instituicoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `instituicoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `dataNascimento` date NOT NULL,
@@ -53,7 +89,24 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `isAdministrador` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-04 18:04:39
