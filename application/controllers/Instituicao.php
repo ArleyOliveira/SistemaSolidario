83e4effe1ffb9108af58cc89638459f0e797f523
MesquitaCosta <arley.msn@hotmail.com>
2016-07-04 17:45:36 -0300

    modificações controller Instituição

--- application/controllers/Instituicao.php
+++ application/controllers/Instituicao.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Doador extends CI_Controller {
+class Instituicao extends CI_Controller {
 
     function __construct() {
         parent:: __construct();
@@ -12,39 +12,38 @@ class Doador extends CI_Controller {
         $this->load->helper('array');
         $this->load->library('table');
         $this->load->library('session');
-        $this->load->model('Doador_model', "DoadorDAO");
+        $this->load->model('Instituicao_model', "InstituicaoDAO");
     }
 
     public function cadastrar() {
         $this->form_validation->set_rules('nome', 'Nome', 'trim|required|max_length[100]');
-        $this->form_validation->set_rules('endereco', 'Senha', 'trim|required|max_length[100]');
-        $this->form_validation->set_rules('telefone', 'Área do conhecimento', 'trim|required|max_length[100]');
-        $this->form_validation->set_rules('dataNascimento', 'Data de Nascimento', 'trim|required|max_length[100]');   
+        $this->form_validation->set_rules('cnpj', 'CNPJ', 'trim|required|max_length[14]');
+        $this->form_validation->set_rules('telefone', 'Área do conhecimento', 'trim|required|max_length[100]');  
         $this->form_validation->set_rules('email', 'Email', 'trim|required|max_length[50]');
-	$this->form_validation->set_rules('senha', 'Senha', 'trim|required');
-        $this->form_validation->set_message('matches', 'O campo %s não corresponde com o campo %s');
-        $this->form_validation->set_rules('senha2', 'Repita a Senha', 'trim|required|matches[senha]');
+        $this->form_validation->set_rules('endereco', 'Endereco', 'trim|required|max_length[100]');
+        $this->form_validation->set_rules('descricao', 'Descricao', 'trim|required|max_length[800]');
+        $this->form_validation->set_rules('isDisponivel','IsDisponivel');
+
          
       
         if ($this->form_validation->run()):
-            $dados = elements(array('nome', 'endereco', 'telefone', 'dataNascimento', 'email', 'senha'), $this->input->post());
-                $dados['senha'] = md5($dados['senha']);
-                $this->DoadorDAO->do_insert($dados);
+            $dados = elements(array('nome', 'cnpj', 'telefone', 'email', 'endereco', 'descricao', 'isDisponivel'), $this->input->post());            
+                $this->InstituicaoDAO->do_insert($dados);
         endif;
         
         $dados = array(
             'titulo' => 'Sistem Solidário',
-            'tela' => 'doadores/cadastrar',
+            'tela' => 'instituicoes/cadastrar',
         );
         $this->load->view("exibirDados", $dados);
     }
     
     public function consultar(){
-        $doadores = $this->DoadorDAO->get_all();
+        $instituicoes = $this->InstituicaoDAO->get_all();
         $dados = array(
             'titulo' => 'Sistema Solidário',
-            'tela' => 'doadores/consultar',
-            'doadores' => $doadores,
+            'tela' => 'instituicoes/consultar',
+            'instituicoes' => $instituicoes,
         );
         $this->load->view("exibirDados", $dados);
     }
