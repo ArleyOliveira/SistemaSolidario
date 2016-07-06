e32e86a1e5532ecd92034350826aaaaa8732ec61
igorguedes <i.guedes93@hotmail.com>
2016-07-06 17:22:13 -0300

    Criação view adm

---
+++ application/views/administrador/consultar.php
@@ -0,0 +1,29 @@
+<?php
+if($this->session->permissao != 1)
+    redirect('administrador/consultar');
+	if($this->session->flashdata('excluirok')):
+			echo '<div class="alert alert-success" role="alert"> <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>  ' .$this->session->flashdata('excluirok') . '</div>';
+		endif;
+?>
+	<br />
+	<div class="panel panel-default">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">Doadores Cadastrados</h3>
+	  </div>
+	  <div class="panel-body">
+	  	<div class="table-responsive"></div>
+<?php
+	$template = array(
+        'table_open' => '<table class="table">'
+	);
+	$this->table->set_template($template);
+	$this->table->set_heading('ID', 'Nome Completo', 'Endereco', 'Telefone', 'Data de Nascimento', 'Email');
+	foreach ($administradores as $linha):
+		$this->table->add_row($linha->id, $linha->nome, $linha->endereco, $linha->dataNascimento, $linha->email, anchor("Administradores/update/$linha->id", 'Editar') . ' - '. anchor("Administradores/delete/$linha->id", 'Deletar'));
+	endforeach;
+	
+	echo $this->table->generate();
+?>
+			</div>
+        </div>
+
