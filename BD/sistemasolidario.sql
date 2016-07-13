cff0846f163badf29bc854dd2af113b2fa373aab
josimarspj <josimarspj33@gmail.com>
2016-07-13 17:04:03 -0300

    criação da tabela necessidades

--- BD/sistemasolidario.sql
+++ BD/sistemasolidario.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 09-Jul-2016 às 22:16
--- Versão do servidor: 10.1.13-MariaDB
--- PHP Version: 5.6.20
+-- Servidor: localhost
+-- Tempo de Geração: 13/07/2016 às 17:02
+-- Versão do servidor: 5.5.47-0ubuntu0.14.04.1
+-- Versão do PHP: 5.5.9-1ubuntu4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,30 +14,31 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `sistemasolidario`
+-- Banco de dados: `sistemasolidario`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `doadores`
+-- Estrutura para tabela `doadores`
 --
 
-CREATE TABLE `doadores` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `doadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(40) NOT NULL,
   `endereco` varchar(150) NOT NULL,
   `email` varchar(40) NOT NULL,
   `senha` varchar(32) NOT NULL,
   `dataNascimento` date NOT NULL,
-  `telefone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `telefone` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Extraindo dados da tabela `doadores`
+-- Fazendo dump de dados para tabela `doadores`
 --
 
 INSERT INTO `doadores` (`id`, `nome`, `endereco`, `email`, `senha`, `dataNascimento`, `telefone`) VALUES
@@ -46,22 +47,25 @@ INSERT INTO `doadores` (`id`, `nome`, `endereco`, `email`, `senha`, `dataNascime
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `instituicoes`
+-- Estrutura para tabela `instituicoes`
 --
 
-CREATE TABLE `instituicoes` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `instituicoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(250) NOT NULL,
   `cnpj` varchar(30) NOT NULL,
   `telefone` varchar(14) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `endereco` varchar(250) NOT NULL,
   `descricao` varchar(250) NOT NULL,
-  `isDisponivel` tinyint(1) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `isDisponivel` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cnpj` (`cnpj`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Extraindo dados da tabela `instituicoes`
+-- Fazendo dump de dados para tabela `instituicoes`
 --
 
 INSERT INTO `instituicoes` (`id`, `nome`, `cnpj`, `telefone`, `email`, `endereco`, `descricao`, `isDisponivel`) VALUES
@@ -70,90 +74,59 @@ INSERT INTO `instituicoes` (`id`, `nome`, `cnpj`, `telefone`, `email`, `endereco
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tipo_doacoes`
+-- Estrutura para tabela `necessidades`
 --
 
-CREATE TABLE `tipo_doacoes` (
+CREATE TABLE IF NOT EXISTS `necessidades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(200) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_validade` date NOT NULL,
+  `id_instituicao` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tipo_doacoes`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_doacoes` (
   `id_doadores` int(11) NOT NULL,
   `roupas` tinyint(1) NOT NULL DEFAULT '0',
   `alimentos` tinyint(1) NOT NULL DEFAULT '0',
   `outros` tinyint(1) NOT NULL DEFAULT '0',
   `tempo` tinyint(1) NOT NULL DEFAULT '0',
-  `moveis` tinyint(1) NOT NULL DEFAULT '0'
+  `moveis` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_doadores`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `usuario`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `dataNascimento` date NOT NULL,
   `email` varchar(30) NOT NULL,
   `senha` varchar(32) NOT NULL,
   `isModerador` tinyint(1) NOT NULL,
-  `isAdministrador` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `isAdministrador` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Fazendo dump de dados para tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `dataNascimento`, `email`, `senha`, `isModerador`, `isAdministrador`) VALUES
 (1, 'Arley Oliveira', '2016-07-15', 'arley.msn@yahoo.com', '202cb962ac59075b964b07152d234b70', 1, 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `doadores`
---
-ALTER TABLE `doadores`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `instituicoes`
---
-ALTER TABLE `instituicoes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cnpj` (`cnpj`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `tipo_doacoes`
---
-ALTER TABLE `tipo_doacoes`
-  ADD PRIMARY KEY (`id_doadores`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `doadores`
---
-ALTER TABLE `doadores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `instituicoes`
---
-ALTER TABLE `instituicoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
